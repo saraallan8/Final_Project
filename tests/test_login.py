9f375f414bf9bd6a16b7121b88c9c99f1aa118e1
sara allan <engsaraallan@gmail.com>
2024-11-05 15:55:21 +0200

    for allure report

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,6 +2,6 @@ from pages.login import LogInPage
 from selenium.webdriver.common.by import By
 
 def test_login(browser):
-        login_page = LogInPage(browser) 
-        login_page.load()  
-        login_page.login()
+ login_page = LogInPage(browser) 
+ login_page.load()  
+ login_page.login()
